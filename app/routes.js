7a86b7318a06173ca64e668b96d19c026ed464aa
Simon Hughesdon <simon.hughesdon@digital.cabinet-office.gov.uk>
2020-08-12 23:18:47 +0100

    Fix search by proxying with query string

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const request = require('request');
+const url = require('url');
 // Add your routes here - above the module.exports line
 
 const BASE_URL = "https://govuk-explore-api-p1.herokuapp.com/"
@@ -28,7 +29,9 @@ router.get('/browse/:topicSlug/:subTopicSlug', function (req, res) {
 
 router.get('/*', function(req,res) {
   //modify the url in any way you want
-  var newurl = 'https://www.gov.uk/' + req.path;
+  var url_parts = url.parse(req.url, false);
+  var query = url_parts.query;
+  var newurl = 'https://www.gov.uk' + req.path + "?" + query;
   request(newurl).pipe(res);
 });
 
